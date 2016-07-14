990d02d21ec098144d9c84929350517959eac83d
manzoor <manzoor.kvana@gmail.com>
2016-07-14 11:40:29 +0530

    secondcommit

--- app/src/main/java/com/androidtutorialpoint/retrofitandroid/MainActivity.java
+++ app/src/main/java/com/androidtutorialpoint/retrofitandroid/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         text_id_1 = (TextView) findViewById(R.id.text_id_1);
         text_name_1 = (TextView) findViewById(R.id.text_name_1);
@@ -67,14 +65,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
     }
 
     void getRetrofitArray() {
